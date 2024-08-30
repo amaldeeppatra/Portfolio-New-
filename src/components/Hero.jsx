54e828cb2750044ce2c4ddd4cc64731f1c18e985
Amaldeep <amaldeeppatra21@gmail.com>
2024-08-30 17:55:59 +0530

    hero text color updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ import profilePic from '/images/profile pic square.png';
 
 const Hero = () => {
   return (
-    <section className='flex flex-col md:flex-row justify-between px-8 md:px-[12rem] py-10 text-[2rem] md:text-[2.5rem] font-ibmMono font-[500] items-center text-[#334257] mb-24'>
+    <section className='flex flex-col md:flex-row justify-between px-8 md:px-[12rem] py-10 text-[2rem] md:text-[2.5rem] font-ibmMono font-[500] items-center mb-24' style={{ color: 'var(--hero-color)' }}>
       <div className='left flex flex-col items-center md:items-start text-center md:text-left'>
         <span className='text-base md:text-lg text-[#548CA8]'>Web Developer | ML Developer</span>
         <p className='font-bold mt-2'>Amaldeep Patra</p>
